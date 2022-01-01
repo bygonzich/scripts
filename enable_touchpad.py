8d48dc557b2da32f69125f641d0f0493044af47f
Gonz <91077603+bygonzich@users.noreply.github.com>
2022-01-01 02:28:32 +0100

    Add files via upload
    This script enables ELAN Touchpad when it is disabled by default and you don't have a mouse to do it.

---
+++ enable_touchpad.py
@@ -0,0 +1,14 @@
+from re import X
+import pyautogui as pg
+import time
+
+pg.press("win")
+pg.write("elan")
+pg.press("enter")
+time.sleep(1.5)
+pg.press("tab",5)
+pg.press("space")
+pg.press("tab",4)
+pg.press("space")
+pg.press("left",2)
+pg.press("space")
